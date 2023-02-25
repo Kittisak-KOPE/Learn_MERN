47e3c71d7301e45ec4e6edaa2fdc7b3baac66872
KOPE <kittisak.kope2021@gmail.com>
2023-02-25 18:48:51 +0700

    Initial REST API goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const {
   getGoals,
-  setGoals,
+  setGoal,
   updateGoal,
   deleteGoal,
 } = require("../controllers/goalController");
 
-router.route("/").get(getGoals).post(setGoals);
+router.route("/").get(getGoals).post(setGoal);
 router.route("/:id").delete(deleteGoal).put(updateGoal);
 
 module.exports = router;
